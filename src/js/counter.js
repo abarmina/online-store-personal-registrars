945e70af4772d33f75e8d906403c2ac3d69df394
abarmina <anutik-barmina@mail.ru>
2020-07-13 14:31:14 +0500

    added price logic

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,13 +1,24 @@
 const decreaseButton = document.getElementById('j-minus'),
       increaseButton = document.getElementById('j-plus'),
+      oldPrice = document.getElementById('old-price'),
+      newPrice = document.getElementById('new-price'),
       counter = document.getElementById('j-counter');
-let counterNumber = Number(counter.textContent);
+let counterNumber = Number(counter.textContent),
+    oldPriceNumber = Number(oldPrice.textContent),
+    newPriceNumber = Number(newPrice.textContent);
+const startOldPrice = oldPriceNumber,
+      startNewPrice = newPriceNumber;
 
 decreaseButton.addEventListener('click', () => {
-    if (counterNumber > 0) counterNumber--;
-    counter.textContent = counterNumber;
+    if (counterNumber < 2) return;
+
+    counter.textContent = --counterNumber;
+    oldPrice.textContent = (oldPriceNumber -= startOldPrice);
+    newPrice.textContent = (newPriceNumber -= startNewPrice);
 });
 
 increaseButton.addEventListener('click', () => {
     counter.textContent = ++counterNumber;
+    oldPrice.textContent = (oldPriceNumber += startOldPrice);
+    newPrice.textContent = (newPriceNumber += startNewPrice);
 });
